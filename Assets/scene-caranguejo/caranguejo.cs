014b6a83d243a2dd2b84e740a83a7a39d35946f5
Tainá Simões <taina.simoes.santos@gmail.com>
2015-08-26 20:48:36 -0300

    crab

---
+++ Assets/scene-caranguejo/caranguejo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class caranguejo : MonoBehaviour {
+
+	public GameObject crab;
+	public int max, min;
+
+
+
+	// Use this for initialization
+	void Start () {
+		crabMoves ();
+	
+	}
+
+	// Crab moves
+	void crabMoves (){
+
+		//Instantiate(crab, randomPosition(), Quaternion.identity);
+
+
+	}
+
+	//Vector3 randomPosition() {
+
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
